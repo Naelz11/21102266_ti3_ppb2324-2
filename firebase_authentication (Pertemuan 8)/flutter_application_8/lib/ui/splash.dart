1ec3fb63622175b6883dd808f8edbd2dcc698b22
Naelz11 <fauziabdiel11@gmail.com>
2024-06-11 00:11:15 +0700

    barulagi

---
+++ firebase_authentication (Pertemuan 8)/flutter_application_8/lib/ui/splash.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.pushNamed(context, '/login'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: NewWidget(),
+      ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Praktikum",
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff3D4DE0),
+          ),
+        ),
+        Text(
+          "PPB",
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff3D4DE0),
+          ),
+        ),
+      ],
+    );
+  }
+}
